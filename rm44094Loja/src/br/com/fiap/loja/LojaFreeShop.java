fb3e15896f8c315bba5ccf8861522c3d6a1464a1
Kleber Silva Rocha <klebersilvarocha@gmail.com>
2013-08-03 17:09:25 -0300

    Versão do dia 03/07

--- rm44094Loja/src/br/com/fiap/loja/LojaFreeShop.java
+++ rm44094Loja/src/br/com/fiap/loja/LojaFreeShop.java
@@ -7,17 +7,29 @@ public class LojaFreeShop {
 	
 	public static void main(String[] args) {
 		
-		MoedaEstrangeira converter = new MoedaEstrangeira();
+		MoedaEstrangeiraProxy converter = new MoedaEstrangeiraProxy();
 		
 		System.out.println("Digite o valor em Dólar: ");
 		
-		Scanner teclado = new Scanner(System.in);
 		
-		int valorDolar = teclado.nextInt();		
+		try {
+			
+			Scanner teclado = new Scanner(System.in);
+			
+			int valorDolar = teclado.nextInt();		
 		
-		int valorReal = converter.converterDolar(valorDolar);
+			int valorReal = converter.converterDolar(valorDolar);
+			
+			System.out.println("Valor em Real: "+ valorReal);
+			
+		} catch (Exception e) {
+			
+			System.out.println("Erro. Tente Mais tarde!");
+			
+		}
 		
-		System.out.println("Valor em Real: "+ valorReal);
+		
+		
 		
 	}
 
